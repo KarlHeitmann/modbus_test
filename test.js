569ff2d8d6ffe81fd607df76285b90f815109561
Karl Heitmann <khf1988@gmail.com>
2019-12-13 18:03:01 -0300

    vuelta a npm, el programa modbus-stream funciona solo con node v10

---
+++ test.js
@@ -0,0 +1,21 @@
+var modbus = require("modbus-stream");
+
+modbus.tcp.server({ debug: "server" }, (connection) => {
+  connection.readCoils({ address: 5, quantity: 8 }, (err, info) => {
+    console.log("response", info.response.data);
+  });
+}).listen(12345, () => {
+  console.log("Listening...")
+  modbus.tcp.connect(12345, { debug: "client" }, (err, connection) => {
+    connection.on("read-coils", (request, reply) => {
+      reply(null, [ 1, 0, 1, 0, 1, 1, 0, 1 ]);
+    });
+  });
+
+  modbus.tcp.connect(12345, { debug: "client" }, (err, connection) => {
+    connection.on("read-coils", (request, reply) => {
+      reply(null, [ 1, 0, 1, 0, 1, 1, 0, 1 ]);
+    });
+  });
+});
+
