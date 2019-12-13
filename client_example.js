bd7913df357190d9729b4e50b0cf5709a26def8d
Karl Heitmann <khf1988@gmail.com>
2019-12-13 11:56:36 -0300

    corregidos ejemplos pero no funcionan bien, agregado el paquete modbus-tcp que aun no hemos probado

--- client_example.js
+++ client_example.js
@@ -2,17 +2,12 @@
 
 // use socket.on('open', ...) when using serialport
 socket.on('connect', function () {
-
-// make some calls
-
-client.readCoils(0, 13).then(function (resp) {
-
-// resp will look like { response : [TCP|RTU]Response, request: [TCP|RTU]Request }
-// the data will be located in resp.response.body.coils: <Array>, resp.response.body.payload: <Buffer>
-
-console.log(resp);
-
-}, console.error);
+  // make some calls
+  client.readCoils(0, 13).then(function (resp) {
+    // resp will look like { response : [TCP|RTU]Response, request: [TCP|RTU]Request }
+    // the data will be located in resp.response.body.coils: <Array>, resp.response.body.payload: <Buffer>
+    console.log(resp);
+  }, console.error);
 
 });
 
--- server_example.js
+++ server_example.js
@@ -1,9 +1,22 @@
+const modbus = require('jsmodbus')
+const net = require('net')
+const server = new net.Server()
+const server_modbus = new modbus.server.TCP(server)
+
+// server_modbus.listen(502)
+// server.listen(502)
+server.listen(1033)
+
+/*
 // create a tcp modbus client
 const Modbus = require('jsmodbus')
 const net = require('net')
 const socket = new net.Socket()
-const client = new Modbus.client.TCP(socket, unitId)
+// const client = new Modbus.client.TCP(socket, unitId)
+const client = new Modbus.client.TCP(socket, 4453)
+/*
 const options = {
-	'host' : host
+	'host' : host,
 	'port' : port
 }
+*/
